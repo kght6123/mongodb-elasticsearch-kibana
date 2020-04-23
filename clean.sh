38b74484d25d78e72ef4b2acdd1472714c7f36f3
Hirotaka <kght6123@live.jp>
2020-04-23 14:08:00 +0000

    fix not mongo-connector running.

--- clean.sh
+++ clean.sh
@@ -1,11 +1,4 @@
-/bin/rm -dfR ./mysql/data/*
-/bin/rm -dfR ./phpmyadmin/sessions/*
-#/bin/rm -dfR ./wordpress/plugins/*
-/bin/rm -dfR ./wordpress/plugins/akismet
-/bin/rm -dfR ./wordpress/plugins/hello.php
-/bin/rm -dfR ./wordpress/plugins/index.php
-#/bin/rm -dfR ./wordpress/themes/*
-/bin/rm -dfR ./wordpress/themes/index.php
-/bin/rm -dfR ./wordpress/themes/twentyfifteen
-/bin/rm -dfR ./wordpress/themes/twentyseventeen
-/bin/rm -dfR ./wordpress/themes/twentysixteen
+rm -dfR ./esdata1/*
+rm -dfR ./mongodb1/*
+rm -dfR ./mongodb2/*
+rm -dfR ./mongodb3/*
